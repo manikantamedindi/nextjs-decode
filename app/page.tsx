b7af92aeb7071e2464d69f05d6cb12134d255aa7
Manikanta Medindi <manikantanani14@gmail.com>
2025-10-28 11:31:33 +0530

    refactor(app): remove dashboard and about pages, simplify home page and clean config
    - Deleted About, Analytics, Users, UserDetails, and Error components
    - Simplified Home page to a static welcome message without data fetching
    - Removed experimental turbopackFileSystemCacheForDev option from next.config.ts
    - Updated electron-to-chromium dependency to version 1.5.241 in package-lock.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Image from "next/image";
 
-export default async function Home() {
-  const data = await fetch("https://microsoftedge.github.io/Demos/json-dummy-data/64KB.json")
-  if(!data.ok) throw new Error("Failed to fetch data");
-  const albums = await data.json();
-
-
+export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <h1>Welcome to Next Js 16</h1>
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental:{
-    turbopackFileSystemCacheForDev: true
-  }
 };
 
 export default nextConfig;
